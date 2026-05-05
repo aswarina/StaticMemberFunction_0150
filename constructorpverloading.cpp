343984783539744ad88dac65ed9d94b778e8c485
aswarina <lyxsyyawa@gmail.com>
2026-05-05 19:45:41 +0700

    mulai isi main program dan buat objek buat mahasiswa satu dan dua

--- constructorpverloading.cpp
+++ constructorpverloading.cpp
@@ -41,3 +41,9 @@ void mahasiswa::cetak()
     cout << endl << "Nim = " << nim << endl;
     cout << "Nama = " << nama << endl;
 }
+
+int main()
+{
+     mahasiswa mhs1(102030); 
+     mahasiswa mhs2("Andi");   
+     mahasiswa mhs3(010203, "Santi"); 
