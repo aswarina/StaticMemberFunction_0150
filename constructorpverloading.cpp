d7294672317f90181cd87e59c01135d9abaedb35
aswarina <lyxsyyawa@gmail.com>
2026-05-05 19:44:26 +0700

    lengkapin constructor dua parameter sama fungsi buat cetak data

--- constructorpverloading.cpp
+++ constructorpverloading.cpp
@@ -29,3 +29,15 @@ mahasiswa :: mahasiswa(string iNama)
 {
     nama = iNama;
 }
+
+mahasiswa :: mahasiswa(int iNim, string iNama)
+{
+    nim = iNim;
+    nama = iNama;
+}
+
+void mahasiswa::cetak() 
+{
+    cout << endl << "Nim = " << nim << endl;
+    cout << "Nama = " << nama << endl;
+}
